b626901f5d157708a8c446f43d05eb4be4ac6066
mjc0025 <91498510+mjc0025@users.noreply.github.com>
2024-02-26 08:02:14 -0600

    Update OOBE_HardwareHash_Upload.ps1

--- OOBE_HardwareHash_Upload.ps1
+++ OOBE_HardwareHash_Upload.ps1
@@ -1,5 +1,5 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 PowerShell.exe -ExecutionPolicy Bypass
-Invoke-WebRequest "https://raw.githubusercontent.com/Auburn-OIT-Cloud/Intune-Microsoft-Graph/main/Get-WindowsAutoPilotInfo.ps1" -OutFile 'C:\Program Files\WindowsPowerShell\Scripts\Get-WindowsAutoPilotInfo.ps1'
+Invoke-WebRequest "https://raw.githubusercontent.com/Auburn-OIT-Cloud/Intune-Microsoft-Graph/main/Get-WindowsAutoPilotInfo.ps1" -OutFile 'C:\Program Files\WindowsPowerShell\Get-WindowsAutoPilotInfo.ps1'
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 Get-WindowsAutopilotInfo -Online
